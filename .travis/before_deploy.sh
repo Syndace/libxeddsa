577f89267bdbc1983a5af301ebb222ddfcc97f36
Tim Henkes <tim.henkes@student.hs-rm.de>
2020-03-26 16:35:32 +0100

    added release deploy configuration to Travis

---
+++ .travis/before_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ${TRAVIS_BUILD_DIR}/bin/
+
+if [ "$TRAVIS_OS_NAME" = "osx" ]
+then
+    mv libxeddsa_static.a      libxeddsa-macos-amd64.a
+    mv libxeddsa_dynamic.dylib libxeddsa-macos-amd64.dylib
+fi
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]
+then
+    mv libxeddsa_static.a   libxeddsa-linux-amd64.a
+    mv libxeddsa_dynamic.so libxeddsa-linux-amd64.so
+fi
