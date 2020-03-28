22f566a6c8fd72fcb2158612e92b0bed821feb0b
Tim Henkes <me@syndace.dev>
2020-03-28 23:40:21 +0100

    building combined libraries now

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -2,19 +2,19 @@ cd ${TRAVIS_BUILD_DIR}/bin/
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]
 then
-    mv libxeddsa_static.a   libxeddsa-linux-amd64.a
-    mv libxeddsa_dynamic.so libxeddsa-linux-amd64.so
+    mv libcombined_static.a   libxeddsa-linux-amd64.a
+    mv libcombined_dynamic.so libxeddsa-linux-amd64.so
 fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]
 then
-    mv libxeddsa_static.a      libxeddsa-macos-amd64.a
-    mv libxeddsa_dynamic.dylib libxeddsa-macos-amd64.dylib
+    mv libcombined_static.a      libxeddsa-macos-amd64.a
+    mv libcombined_dynamic.dylib libxeddsa-macos-amd64.dylib
 fi
 
 if [ "$TRAVIS_OS_NAME" = "windows" ]
 then
-    mv xeddsa_static.lib  libxeddsa-windows-amd64.lib
-    mv xeddsa_dynamic.lib libxeddsa-windows-amd64.dll.lib
-    mv xeddsa_dynamic.dll libxeddsa-windows-amd64.dll
+    mv combined_static.lib  libxeddsa-windows-amd64.lib
+    mv combined_dynamic.lib libxeddsa-windows-amd64.dll.lib
+    mv combined_dynamic.dll libxeddsa-windows-amd64.dll
 fi
